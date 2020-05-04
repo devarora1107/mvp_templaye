c65308dd7fb6738f6aa112aec01dc2ccf40be552
devarora arora <devarora@outlook.com>
2020-05-04 16:51:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,21 +7,34 @@ import {Row} from 'react-bootstrap';
 import CaroselHorizontal from '../src/Containers/CarouselHorizontal';
 import Slidder from '../src/Containers/Slidder'
 import { Container } from '@material-ui/core';
+import { Switch, Route, Redirect } from "react-router-dom";
+import privacy from '../src/Containers/privacy'
+const mainComponent = ()=>{
+    return (<React.Fragment>
+        <Container maxWidth="sm">
+            <Header/>
+        </Container>
+        {/*x*/}
+
+        <CaroselHorizontal/>
+    </React.Fragment>)
+}
+
 function App() {
   return (
       <React.Fragment>
     {/*// <div className="App">*/}
     {/*  */}
-          <Container maxWidth="sm">
-        <Header/>
-          </Container>
-          {/*x*/}
+       <Switch>
+           <Route exact = '/privacy-policy' component={privacy}/>
+           <Route exact='/' component={ mainComponent}/>
 
-        <CaroselHorizontal/>
+
     {/*  // <Row>*/}
     {/*  // <Template1/>*/}
     {/*  // </Row>*/}
     {/*// </div>*/}
+       </Switch>
     </React.Fragment>
   );
 }
